580d9e0989c62358ac0544c5c1ca903b527a247e
Ally <ally@allyelle.com>
2015-04-07 00:12:07 +0200

    update stage url in config

--- craft/config/general.php
+++ craft/config/general.php
@@ -20,7 +20,7 @@
 
     'stage' => array(
       // Config overrides for our staging environment
-      'siteUrl' => 'serabeena.eu1.frbit.net',
+      'siteUrl' => 'http://serabeena.eu1.frbit.net',
       'devMode' => true,
       'omitScriptNameInUrls' => true,
     ),
